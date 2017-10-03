cd57d5a163ab3715b29ae67ce5424ee2ec84c4a5
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-10-03 15:49:01 -0400

    modified the proof of existence contract to be able to check multiple documents stored in an array

--- contracts/ProofOfExistence1.sol
+++ contracts/ProofOfExistence1.sol
@@ -2,16 +2,40 @@ pragma solidity ^0.4.4;
 
 contract ProofOfExistence1 {
   //state
-  bytes32 public proof;
+  bytes32[] public proofs;
+
+  //store a proof of existence in the contract state
+  //*transactional function*
+  function storeProof(bytes32 proof) public {
+    proofs.push(proof);
+  }
 
   //calculate proof of document
   //*transactional function*
   function notarize(string document) public {
-    proof = calculateProof(document);
+    var proof = calculateProof(document);
+    storeProof(proof);
   }
+
   //helper function to get document's sha256
   //*read-only function*
   function calculateProof(string document) public constant returns (bytes32) {
     return sha256(document);
+  }
+
+  //check if a document has been notarized
+  //*read only function*
+  function checkDocument(string document) public constant returns (bool) {
+    var proof = calculateProof(document);
+    return hasProof(proof);
+  }
+
+  function hasProof(bytes32 proof) public constant returns (bool) {
+    for(uint i = 0; i < proofs.length; i++) {
+      if(proofs[i] == proof) {
+        return true;
+      }
+    }
+    return false;
   }
 }
